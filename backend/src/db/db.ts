3250e98bbcbcb70f962683ef8b9a68c79a5e6a97
caiocsm-sudo <stampedesw29@gmail.com>
2023-12-30 19:25:27 -0300

    deleted old folder

--- backend/src/db/db.ts
+++
@@ -1,9 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-require("dotenv").config();
-
-const projectUrl = process.env.SUPABASE_PROJECT_URL as string;
-const apiKey = process.env.SUPABASE_API_KEY as string;
-
-const supabase = createClient(projectUrl, apiKey);
-
-module.exports = supabase;
