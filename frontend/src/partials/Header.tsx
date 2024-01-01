f69855047771f086d0ee5465ac980b1960f0cbf3
caiocsm-sudo <stampedesw29@gmail.com>
2024-01-01 09:17:23 -0300

    new header at left + colors from hyperborea

--- frontend/src/partials/Header.tsx
+++ frontend/src/partials/Header.tsx
@@ -1,15 +1,26 @@
-import { PersonIcon, HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { PersonIcon, ExitIcon, HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { Link } from "react-router-dom";
+import hyperboreaWhite from "/hyperborea-black.png";
 import "./Header.scss";
+import { useState } from "react";
 
-export default function Header(): JSX.Element {
+const Header = () => {
+  const [isLogged, _setIsLogged] = useState<boolean>(false);
+
   return (
     <header className="header">
-      <h1>Hyperborea Audio</h1>
+      <div className="img-container">
+        <Link to="/">
+          <img src={hyperboreaWhite} alt="hpb logo" />
+        </Link>
+      </div>
       <nav>
-        <button className="default-btn transparent-btn hidden resp-nav-btn"><HamburgerMenuIcon /></button>
+        <button className="default-btn transparent-btn hidden resp-nav-btn">
+          <HamburgerMenuIcon />
+        </button>
         <ul className="ulist">
           <li>
-            <a href="">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
             <a href="">Store</a>
@@ -22,7 +33,20 @@ export default function Header(): JSX.Element {
           </li>
         </ul>
       </nav>
-      <button className="transparent-btn default-btn"><PersonIcon/></button>
+      <div className="buttons">
+        <button className="transparent-btn default-btn">
+          <PersonIcon />
+        </button>
+        {isLogged ? (
+          <button className="transparent-btn default-btn">
+            <ExitIcon />
+          </button>
+        ) : (
+          ""
+        )}
+      </div>
     </header>
   );
-}
+};
+
+export default Header;
