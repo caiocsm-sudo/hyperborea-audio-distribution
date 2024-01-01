fa2d2e8233824d7fda68518aac517e22ea385a3e
caiocsm-sudo <stampedesw29@gmail.com>
2024-01-01 09:16:50 -0300

    react router implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import Header from "./partials/Header";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Header></Header>
-    </>
+    <Router>
+      <main className="main-app">
+        <Header></Header>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
