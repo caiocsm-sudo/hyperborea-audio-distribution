62ba6bec54a672932e13121adce8a35207f703ea
caiocsm-sudo <stampedesw29@gmail.com>
2024-01-03 15:54:09 -0300

    mongodb connecting with mongoose ODM

--- utils/db.ts
+++ utils/db.ts
@@ -1,27 +1,12 @@
-import { MongoClient, Db } from "mongodb";
+import mongoose from "mongoose";
 
-// type Database = { client: MongoClient; db: Db };
-
-let cachedClient: MongoClient;
-let cachedDb: Db;
-
-const connectToDb = async () => {
-  if (cachedClient && cachedDb) {
-    return { client: cachedClient, db: cachedDb };
+const connectDb = async (mongoURI: string) => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to database");
+  } catch (error) {
+    throw new Error("Failed to connect to MongoDB: " + error);
   }
+}
 
-  const connectionString = process.env.MONGODB_URI as string;
-  const dbName = process.env.MONGODB_DB as string;
-
-  if (!connectionString || !dbName) throw new Error("Not able to connect to database");
-
-  const client = await MongoClient.connect(connectionString);
-  const db = client.db(dbName);
-
-  cachedClient = client;
-  cachedDb = db;
-
-  return { client, db };
-};
-
-export default connectToDb;
+export default connectDb;
