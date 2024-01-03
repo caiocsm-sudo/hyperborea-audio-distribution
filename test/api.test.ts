27aee23472320cb8edcaecd719331bd7102e5ca2
caiocsm-sudo <stampedesw29@gmail.com>
2024-01-03 15:51:21 -0300

    unused test

--- test/api.test.ts
+++
@@ -1,9 +0,0 @@
-import { DELETE } from "@/app/api/test/route";
-
-describe("", () => {
-  it("should console.log the request", () => {
-    const spyLog = jest.spyOn(console, "log");
-
-    expect(spyLog).toHaveBeenCalled()
-  });
-});
