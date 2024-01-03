8bfc9b1cf18a194f360ca8dd670f2dd293c0daf1
caiocsm-sudo <stampedesw29@gmail.com>
2024-01-03 15:51:14 -0300

    trying to test next js api routes

--- app/api/test/route.test.ts
+++ app/api/test/route.test.ts
@@ -1,7 +1,24 @@
-describe("porra", () => {
-  it("should return", () => {
-    const spyLog = jest.spyOn(console, "log");
+import type { NextApiRequest, NextApiResponse } from "next";
 
-    expect(spyLog).toHaveBeenCalled();
+import { createMocks } from "node-mocks-http";
+import request from "supertest";
+import handler from "./route";
+import connectDb from "@/utils/db";
+
+describe("api routing test", () => {
+  beforeAll(async () => {
+    connectDb(process.env.MONG0DB_URI as string);
+  });
+  
+  it("should console.log", async () => {
+    const { req, res }: { req: NextApiRequest, res: NextApiResponse } = createMocks({
+      method: "GET",
+      headers: { "content-type": "application/json" }
+    });
+    const spyLog = jest.spyOn(console, "log");
+ 
+    await request(handler).get("/").send(req);
+
+    expect(spyLog).toHaveBeenCalledTimes(1);
   });
 });
