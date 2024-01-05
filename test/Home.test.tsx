738567a08ebfe0c46d9c89fbf8d168cc3173bb36
caiocsm-sudo <stampedesw29@gmail.com>
2024-01-05 14:27:05 -0300

    home test

--- test/Home.test.tsx
+++ test/Home.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
 import Home from "@/app/page";
 
-it.only("should have been rendered", () => {
-  render(<Home />);
+describe("testing if there's any reneder issue", () => {
+  it.only("should have been rendered", () => {
+    render(<Home />);
 
-  expect(screen.getByText("Oi galera")).toBeInTheDocument();
+    expect(screen.getByText("Oi galera")).toBeInTheDocument();
+  });
 });
-
