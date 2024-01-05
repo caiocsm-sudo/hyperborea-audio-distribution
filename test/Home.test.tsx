2f09457e160ce0a4d03c73215b1656dd5db97cb2
caiocsm-sudo <stampedesw29@gmail.com>
2024-01-05 14:28:48 -0300

    test passing

--- test/Home.test.tsx
+++ test/Home.test.tsx
@@ -7,6 +7,6 @@ describe("testing if there's any reneder issue", () => {
   it.only("should have been rendered", () => {
     render(<Home />);
 
-    expect(screen.getByText("Oi galera")).toBeInTheDocument();
+    expect(screen.getByText("Hyperborea Sound Distribution")).toBeInTheDocument();
   });
 });
