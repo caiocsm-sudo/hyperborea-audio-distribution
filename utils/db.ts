3f69991de8a15cd3f5cfb4dbb5cbb91df68b52d5
caiocsm-sudo <stampedesw29@gmail.com>
2024-01-09 01:25:42 -0300

    changed connection string to prevent test database

--- utils/db.ts
+++ utils/db.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from "mongoose"
+import dotenv from "dotenv"
 
-dotenv.config({ path: ".env.local" });
+dotenv.config({ path: ".env.local" })
 
 const connectDb = async () => {
   try {
-    const mongoURI = `${process.env.MONGO_URI}`;
-    await mongoose.connect(mongoURI);
+    const mongoURI = `${process.env.MONGO_URI}`
+    await mongoose.connect(mongoURI)
 
-    console.log("connected to the database");
+    console.log("connected to the database")
   } catch (error) {
-    throw new Error("Failed to connect to MongoDB: " + error);
+    throw new Error("Failed to connect to MongoDB: " + error)
   }
 }
 
-export default connectDb;
+export default connectDb
