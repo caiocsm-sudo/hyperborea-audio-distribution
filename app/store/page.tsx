0557475efc03fe22e4ddb08fba7cb17da58e8d50
caiocsm-sudo <stampedesw29@gmail.com>
2024-01-09 01:28:48 -0300

    no significant change

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,11 +1,10 @@
 export default function Store() {
-  return(
+  return (
     <section className="hpb-store">
       <h1>Hyperborea official discography</h1>
       <section className="grid-albums">
         <div>Is this too niggerlicious or is this divine intellect</div>
       </section>
     </section>
-  );
+  )
 }
-
