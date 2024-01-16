57cee4561694a70bc0744e50cbfa4c44a30f4a90
caiocsm-sudo <stampedesw29@gmail.com>
2024-01-16 10:27:49 -0300

    user profile route

---
+++ app/(user)/profile/page.tsx
@@ -0,0 +1,12 @@
+const Profile = (): JSX.Element => {
+  // const userInfo = fetch("/api/user/profile");
+  return(
+    <section className="user">
+      <header className="user__profile">
+        <div>user name</div>
+      </header>
+    </section>
+  )
+}
+
+export default Profile
